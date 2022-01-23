0cfa40f7256841d17d0e314ba7004a5c5d14ace9
Abhinandan <abhinandans109@gmail.com>
2022-01-23 10:13:45 +0530

    final

--- app/src/main/java/com/example/ebookepup/MainActivity.kt
+++ app/src/main/java/com/example/ebookepup/MainActivity.kt
@@ -1,32 +1,40 @@
 package com.example.ebookepup
 
-import android.R.attr
+import android.Manifest
 import android.annotation.SuppressLint
-import android.app.Activity
-import android.app.PendingIntent.getActivity
 import android.content.ActivityNotFoundException
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.widget.Button
+import com.folioreader.Config
 import com.folioreader.FolioReader
-import android.widget.Toast
+import com.folioreader.Constants
+import java.io.File
+import androidx.core.app.ActivityCompat
 
-import android.content.Intent
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.net.Uri
+
+import android.os.Build
 import android.provider.OpenableColumns
-import java.io.File
-import android.R.attr.data
 import android.util.Log
-import android.provider.MediaStore
-import androidx.loader.content.CursorLoader
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        showFileChooser()
+        val s="helo"
 
+
+        val button : Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            showFileChooser()
+        }
 
     }
     private fun showFileChooser() {
@@ -52,13 +60,14 @@ class MainActivity : AppCompatActivity() {
             1 -> if (resultCode === RESULT_OK) {
                 // Get the Uri of the selected file
                 val uri: Uri? = data!!.data
-                val uriString ="file:/" +uri!!.path.toString()
+                val uriString =uri!!.path.toString()
                 Log.e("filex",uriString)
+
                 val folioReader = FolioReader.get()
                 val myFile = File(uriString)
-                val pathf =getRealPathFromUri(uri!!)
-                val path = myFile.absolutePath
-                folioReader.openBook(path)
+                var path = myFile.canonicalPath
+                path=path.split(":")[1]
+                isReadStoragePermissionGranted(path)
                 var displayName: String? = null
                 if (uriString.startsWith("content://")) {
                     var cursor: Cursor? = null
@@ -82,14 +91,78 @@ class MainActivity : AppCompatActivity() {
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
-    private fun getRealPathFromUri(uri: Uri): String? {
-        val projection = arrayOf(MediaStore.Images.Media.DATA)
-        val cursorLoader = CursorLoader(applicationContext, uri, projection, null, null, null)
-        val cursor: Cursor = cursorLoader.loadInBackground()!!
-        val column = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-        cursor.moveToFirst()
-        val result = cursor.getString(column)
-        cursor.close()
-        return result
+
+    fun isReadStoragePermissionGranted(file:String?): Boolean {
+        return if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                Log.v("TAG", "Permission is granted1")
+                val config: Config = Config()
+                    .setAllowedDirection(Config.AllowedDirection.ONLY_VERTICAL)
+                    .setDirection(Config.Direction.VERTICAL)
+                    .setFont(Constants.FONT_LORA)
+                    .setFontSize(5)
+                    .setNightMode(true)
+                    .setShowTts(true)
+
+                val folioReader = FolioReader.get()
+                val path = File(Environment.getExternalStorageDirectory(),file!!)
+                val paths = path.path
+                folioReader.openBook(paths)
+                true
+            } else {
+                Log.v("TAG", "Permission is revoked1")
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    3)
+                Toast.makeText(this,"Permission not Granted",Toast.LENGTH_SHORT).show()
+                false
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+            Log.v("TAG", "Permission is granted1")
+            true
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            2 -> {
+                Log.d("TAG", "External storage2")
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.v("TAG", "Permission: " + permissions[0] + "was " + grantResults[0])
+                    //resume tasks needing this permission
+                    val config: Config = Config()
+                        .setAllowedDirection(Config.AllowedDirection.ONLY_VERTICAL)
+                        .setDirection(Config.Direction.VERTICAL)
+                        .setFont(Constants.FONT_LORA)
+                        .setFontSize(5)
+                        .setNightMode(true)
+                        .setShowTts(true)
+
+                    val folioReader = FolioReader.get()
+                    val path = File(Environment.getExternalStorageDirectory(),"qbc.epub")
+                    val paths = path.path
+                    folioReader.openBook(paths)
+
+                } else {
+                    Toast.makeText(this,"Permission not Granted",Toast.LENGTH_SHORT).show()
+                }
+            }
+            3 -> {
+                Log.d("TAG", "External storage1")
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.v("TAG", "Permission: " + permissions[0] + "was " + grantResults[0])
+                    //resume tasks needing this permission
+
+                } else {
+
+                }
+            }
+        }
     }
 }
